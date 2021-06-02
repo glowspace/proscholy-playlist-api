30a120720117ab8c6416845e3b3a455a7ba913e2
Michael Dojčár <michaeldojcar@gmail.com>
2021-06-03 00:15:10 +0200

    ZPS-424 codeception playlist tests

--- app/Models/Playlist.php
+++ app/Models/Playlist.php
@@ -9,5 +9,33 @@ class Playlist extends Model
 {
     use HasFactory;
 
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 
+
+    public function group()
+    {
+        return $this->belongsTo(Group::class);
+    }
+
+
+    public function playlist_records()
+    {
+        return $this->hasMany(PlaylistRecord::class, 'playlist_id');
+    }
+
+
+    public function addSongLyricRecord(int $song_lyric_id, $title = null): PlaylistRecord
+    {
+        $record                = new PlaylistRecord();
+        $record->playlist_id   = $this->id;
+        $record->song_lyric_id = $song_lyric_id;
+        $record->title         = $title;
+        $record->order         = 1;
+        $record->save();
+
+        return $record;
+    }
 }
